bf04a1bbd8f815ed0db785b6c49293e0466773d7
l3vith <smitbarve666@gmail.com>
2024-09-26 01:46:11 +0530

    OpenGL Boilerplate

---
+++ build/CMakeFiles/MyOpenGLProject.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyOpenGLProject.dir/lib/glad.c.o"
+  "CMakeFiles/MyOpenGLProject.dir/lib/glad.c.o.d"
+  "CMakeFiles/MyOpenGLProject.dir/src/main.cpp.o"
+  "CMakeFiles/MyOpenGLProject.dir/src/main.cpp.o.d"
+  "MyOpenGLProject"
+  "MyOpenGLProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/MyOpenGLProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/MyOpenGLProject.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for MyOpenGLProject.
+# This may be replaced when dependencies are built.
